501c60596f503d33370f8dad58a582f9282950f6
Paul White <whiteblanc@hotmail.com>
2025-11-15 15:01:57 +0000

    initial commit following Spring Initializr

---
+++ src/main/java/io/github/whiteblanc/investor_play/InvestorPlayApplication.java
@@ -0,0 +1,13 @@
+package io.github.whiteblanc.investor_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/whiteblanc/investor_play/InvestorPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.whiteblanc.investor_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
